643cc478b518730f5df09b57620cf98a03dc403a
Simon Samuel <salviensky@gmail.com>
2022-11-21 21:55:33 +0100

    Setup network for mumbai and set path to client

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: { url: ALCHEMY_API_URL, accounts: [METAMASK_PRIVATE_KEY!] },
+  },
+  paths: {
+    artifacts: "./client/artifacts",
+  },
 };
 
 export default config;
