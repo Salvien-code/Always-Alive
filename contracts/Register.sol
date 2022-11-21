ff512e2a5948a791584d8ee3eb901f66b382e9ba
Simon Samuel <salviensky@gmail.com>
2022-11-21 22:56:08 +0100

    Initiated the register function

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -3,7 +3,8 @@
 pragma solidity ^0.8.0;
 
 contract Register {
-    address owner;
+    address public owner;
+    uint256 public MIN_AMOUNT = 0.1 ether;
 
     struct kin {
         address kinAddress;
@@ -13,8 +14,17 @@ contract Register {
 
     mapping(address => kin) kinship;
 
+    event registered();
+
     // mapping (address => );
     constructor() {
         owner = msg.sender;
+    }
+
+    function register() public payable {
+        require(
+            msg.value >= MIN_AMOUNT,
+            "You have to send a minimum of 0.1 MATIC."
+        );
     }
 }
