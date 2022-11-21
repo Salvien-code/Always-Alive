dd57300b6eb2e3ea8b01690f2b2179dac8de988b
Simon Samuel <salviensky@gmail.com>
2022-11-21 22:36:59 +0100

    Created a basic mapping between user and kin

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -3,5 +3,18 @@
 pragma solidity ^0.8.0;
 
 contract Register {
-    constructor() {}
+    address owner;
+
+    struct kin {
+        address kinAddress;
+        uint256 kinAmount;
+        uint256 lockInterval;
+    }
+
+    mapping(address => kin) kinship;
+
+    // mapping (address => );
+    constructor() {
+        owner = msg.sender;
+    }
 }
