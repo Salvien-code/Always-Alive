5b5217311be6d3fe38ae3335dff331ed18ad9a29
Simon Samuel <salviensky@gmail.com>
2022-11-22 13:00:05 +0100

    Created the Register function (Untested)

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -2,17 +2,31 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @author Simon Samuel
+ */
+
 contract Register {
     address public owner;
     uint256 public MIN_AMOUNT = 0.1 ether;
 
     struct kin {
+        // The Contract address of the next of kin.
         address kinAddress;
+        // The deposited amount to be sent to the next of kin.
         uint256 kinAmount;
+        // The interval for locking the funds in the contract.
         uint256 lockInterval;
+        // This checks to see if the user is still alive.
+        bool validationOfLife;
+        // Sets the max number of times the user can validate life.
+        uint16 maxNumberOfConfirmations;
+        // Will send kinAmount to kinAddress if this == maxNumberOfConfirmations
+        uint16 currNumberOfConfirmations;
     }
 
     mapping(address => kin) kinship;
+    address[] public users;
 
     event registered();
 
@@ -21,10 +35,18 @@ contract Register {
         owner = msg.sender;
     }
 
-    function register() public payable {
+    function register(address _kinAddress, uint256 _lockInterval)
+        public
+        payable
+    {
         require(
             msg.value >= MIN_AMOUNT,
             "You have to send a minimum of 0.1 MATIC."
         );
+        kinship[msg.sender].kinAddress = _kinAddress;
+        kinship[msg.sender].lockInterval = _lockInterval;
+        kinship[msg.sender].kinAmount = msg.value;
+
+        users.push(msg.sender);
     }
 }
