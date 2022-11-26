48dc029923ac7da6c2aa9e7c6114a7c867cdb9c9
Simon Samuel <salviensky@gmail.com>
2022-11-26 12:46:26 +0100

    Added contract address and contract abi

--- client/constants.ts
+++ client/constants.ts
@@ -1,5 +1,12 @@
+import alwaysAliveData from "./artifacts/contracts/AlwaysAlive.sol/AlwaysAlive.json";
+
 export const PAID_NFT_URL =
   "https://bafkreiejj34bgkxeo4kmamoeyjxoclmimzosnmk3nlvz2laqwmyw5seutu.ipfs.nftstorage.link/";
 
 export const UNPAID_NFT_URL =
   "https://bafkreihfifnp3hqdwj4uyp3lpv3zpa2ej546yqombbn4j2xgzqnramyydm.ipfs.nftstorage.link/";
+
+export const ALWAYS_ALIVE_ADDRESS =
+  "0xc96AC9C0dFe3AE75808a053bA00F2E50aB275e82";
+
+export const ALWAYS_ALIVE_ABI = alwaysAliveData.abi;
