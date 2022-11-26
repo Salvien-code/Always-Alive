517ddf7b6ae7fb97c3827494bccda23c1e4a17ba
Simon Samuel <salviensky@gmail.com>
2022-11-26 15:38:54 +0100

    Added Connect Button and Logo

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -13,17 +13,20 @@ export default function Home() {
         <link rel="icon" href="/AA_logo.png" />
       </Head>
 
-      <header>
+      <header className={styles.header}>
+        <Image
+          height={300}
+          width={300}
+          src="/Always_Alive_Logo.svg"
+          alt="Always Alive Logo"
+        ></Image>
         <ConnectButton />
       </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Always Alive</h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
+          A Truly Decentralized Medium to send Funds to your Next of Kin.
         </p>
 
         <div className={styles.grid}>
@@ -36,39 +39,16 @@ export default function Home() {
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://simon-samuel.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Made with &#9829; by Simon Samuel
         </a>
       </footer>
     </div>
