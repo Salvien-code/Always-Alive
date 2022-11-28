7bc41699eb75d94462979c905ba8691846c2aed9
Simon Samuel <salviensky@gmail.com>
2022-11-29 00:00:13 +0100

    User can register through frontend

--- client/components/Registration.tsx
+++ client/components/Registration.tsx
@@ -1,21 +1,73 @@
+import { ethers } from "ethers";
+import { useState } from "react";
+import { usePrepareContractWrite, useContractWrite, useAccount } from "wagmi";
 import styles from "../styles/Registration.module.css";
+import { ALWAYS_ALIVE_ABI, ALWAYS_ALIVE_ADDRESS } from "../utils/constants";
 
 function Registration() {
+  const [amount, setAmount] = useState<string>("");
+  const [kinAddress, setKinAddress] = useState<string>();
+
+  const { config } = usePrepareContractWrite({
+    address: ALWAYS_ALIVE_ADDRESS,
+    abi: ALWAYS_ALIVE_ABI,
+    chainId: 80001,
+    functionName: "register",
+    args: [kinAddress],
+    overrides: {
+      value:
+        amount == ""
+          ? ethers.BigNumber.from("0")
+          : ethers.utils.parseEther(amount),
+    },
+  });
+
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    onError(error) {
+      window.alert(`Error: ${error}`);
+    },
+    onSuccess(data) {
+      window.alert(`Registered successfully ${data}`);
+    },
+  });
+
   return (
     <div>
-      <h1 className={styles.h1}>Register Here!</h1>
       <div>
+        <h1 className={styles.h1}>Register Here!</h1>
         <p className={styles.description}>
-          Enter the Amount to Lock for your Kin:
+          Enter the Amount to Lock for your Kin. Minimum is 0.001 MATIC.
         </p>
-        <input className={styles.input} placeholder="Value in MATIC" />
+        <form>
+          <label>
+            Enter Amount:
+            <input
+              className={styles.input}
+              type="number"
+              step="0.001"
+              onChange={(event) => setAmount(event.target.value)}
+              value={amount}
+            />
+          </label>
+          <label>
+            Enter Kin Address:
+            <input
+              className={styles.input}
+              type="text"
+              onChange={(event) => setKinAddress(event.target.value)}
+              value={kinAddress}
+            />
+          </label>
+        </form>
 
-        <p className={styles.description}>
-          Enter the wallet Address of your Kin:
-        </p>
-        <input className={styles.input} placeholder="Enter Kin Address" />
-
-        <button className={styles.button}>Register</button>
+        <button
+          disabled={!write}
+          className={styles.button}
+          onClick={() => write!()}
+        >
+          {!isLoading ? "Register" : "Loading..."}
+        </button>
       </div>
     </div>
   );
