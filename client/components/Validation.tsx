7b3bf85898056f128620777634e84ad5163f7c44
Simon Samuel <salviensky@gmail.com>
2022-11-29 00:28:26 +0100

    Added the getConfirmations to UI

--- client/components/Validation.tsx
+++ client/components/Validation.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { useAccount, useContractRead } from "wagmi";
 import styles from "../styles/Validation.module.css";
+import { ALWAYS_ALIVE_ABI, ALWAYS_ALIVE_ADDRESS } from "../utils/constants";
 
 function Validation() {
+  const { address } = useAccount();
+
+  const { data, isError, isLoading } = useContractRead({
+    address: ALWAYS_ALIVE_ADDRESS,
+    abi: ALWAYS_ALIVE_ABI,
+    functionName: "getCurrentConfirmations",
+    args: [address],
+  });
+
   return (
     <div>
       <h1 className={styles.h1}>Validation</h1>
       <button className={styles.button}>Validate Life</button>
       <p className={styles.description}>Reset your Confirmations to zero.</p>
 
-      <button className={styles.button}>Get Confirmations</button>
-      <p className={styles.description}>Get your current Confirmations count</p>
+      <p className={styles.description}>
+        Your current Confirmations count: {data as number}
+      </p>
     </div>
   );
 }
