22ae8fdebbf8f4867b7098d874e86c43985da08d
Simon Samuel <salviensky@gmail.com>
2022-11-28 17:01:52 +0100

    This order looks better.

--- client/components/Validation.tsx
+++ client/components/Validation.tsx
@@ -4,11 +4,11 @@ function Validation() {
   return (
     <div>
       <h1 className={styles.h1}>Validation</h1>
-      <p className={styles.description}>Reset your Confirmations to zero.</p>
       <button className={styles.button}>Validate Life</button>
+      <p className={styles.description}>Reset your Confirmations to zero.</p>
 
-      <p className={styles.description}>Get your current Confirmations count</p>
       <button className={styles.button}>Get Confirmations</button>
+      <p className={styles.description}>Get your current Confirmations count</p>
     </div>
   );
 }
