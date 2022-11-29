21f754cfffec6d317dee3e8e0b690fde4768907d
Simon Samuel <salviensky@gmail.com>
2022-11-29 13:14:34 +0100

    Validation works correctly now.

--- client/components/Validation.tsx
+++ client/components/Validation.tsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
-import { useAccount, useContractRead } from "wagmi";
+import {
+  useAccount,
+  useContractRead,
+  usePrepareContractWrite,
+  useContractWrite,
+} from "wagmi";
 import styles from "../styles/Validation.module.css";
 import { ALWAYS_ALIVE_ABI, ALWAYS_ALIVE_ADDRESS } from "../utils/constants";
 
 function Validation() {
   const { address } = useAccount();
 
-  const { data, isError, isLoading } = useContractRead({
+  const { config } = usePrepareContractWrite({
+    address: ALWAYS_ALIVE_ADDRESS,
+    abi: ALWAYS_ALIVE_ABI,
+    chainId: 80001,
+    functionName: "validateLife",
+  });
+
+  const {
+    data: writeData,
+    isLoading,
+    write,
+  } = useContractWrite({
+    ...config,
+    onError(error) {
+      window.alert(`Error: ${error}`);
+    },
+    onSuccess(data) {
+      window.alert(`Registered successfully ${data}`);
+    },
+  });
+
+  const { data: readData } = useContractRead({
     address: ALWAYS_ALIVE_ADDRESS,
     abi: ALWAYS_ALIVE_ABI,
     functionName: "getCurrentConfirmations",
@@ -16,11 +42,17 @@ function Validation() {
   return (
     <div>
       <h1 className={styles.h1}>Validation</h1>
-      <button className={styles.button}>Validate Life</button>
+      <button
+        disabled={!write}
+        className={styles.button}
+        onClick={() => write!()}
+      >
+        {!isLoading ? "Validate Life" : "Loading..."}
+      </button>
       <p className={styles.description}>Reset your Confirmations to zero.</p>
 
       <p className={styles.description}>
-        Your current Confirmations count: {data as number}
+        Your current Confirmations count: {readData as number}
       </p>
     </div>
   );
